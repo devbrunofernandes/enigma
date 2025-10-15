e3ebb6edd873862835ad28520332b051befc6340
Bruno Vilas Boas Fernandes <brunovbf2006@gmail.com>
2025-10-15 11:34:40 -0300

    Create EnigmaConfig struct object

--- enigmaConfig.hpp
+++ enigmaConfig.hpp
@@ -1,3 +1,9 @@
+#include <vector>
+#include <string>
+
 struct EnigmaConfig {
-    
+    std::vector<std::string> rotorNames = {"I", "II", "III"};
+    std::string rotorInitial = "AAA";
+    std::string plugboardConnections = "";
+    std::string reflector = "B";
 };
